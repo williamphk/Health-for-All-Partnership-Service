e860e32a6726eb5a2480b71a1726dc42cfa73db2
William Poon <willphk@outlook.com>
2023-11-18 15:03:53 -0500

    Add share button animation

--- script.js
+++ script.js
@@ -123,3 +123,14 @@ wrapper.addEventListener("mouseleave", autoPlay);
 //   document.getElementById("faq-q-volunteer").style.display = "none";
 //   document.getElementById("faq-q-provider").style.display = "flex";
 // });
+
+const shareButton = document.querySelector(".share-btn");
+
+shareButton.addEventListener("mouseleave", () => {
+  shareButton.classList.add("reverse");
+
+  // Optional: Remove the class after animation completes
+  setTimeout(() => {
+    shareButton.classList.remove("reverse");
+  }, 1000); // 1000 ms = 1s (duration of your animation)
+});
