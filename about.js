3f5650a1a8d6805262f26563f37fe58972fea999
William Poon <willphk@outlook.com>
2023-11-19 01:58:26 -0500

    Update colors and add active state to trapezoids

--- about.js
+++ about.js
@@ -8,3 +8,12 @@ shareButton.addEventListener("mouseleave", () => {
     shareButton.classList.remove("reverse");
   }, 1000); // 1000 ms = 1s (duration of your animation)
 });
+
+const trapezoids = document.querySelectorAll(".trapezoid");
+
+trapezoids.forEach((trapezoid) => {
+  trapezoid.addEventListener("click", () => {
+    trapezoids.forEach((t) => t.classList.remove("active"));
+    trapezoid.classList.add("active");
+  });
+});
